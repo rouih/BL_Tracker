820c1affaa90149337cc55211bdf3f7f702238f7
gilkag2 <gilkag2@gmail.com>
2022-09-08 12:11:30 +0300

    created addUser route which creates a json file for the user to hold his stats and credentials

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import { config } from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import axios from 'axios';
+import { writeFile } from 'fs';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 config();
 
 const app = express();
@@ -14,11 +19,37 @@ app.use(bodyParser.json());
 const X_RAPID_API_HOST = "call-of-duty-vanguard.p.rapidapi.com";
 
 
+const saveUserDataToAFile = (userData) => {
+    // console.log(__dirname);
+    writeFile(`${__dirname}/users/${userData.userCredentials.userName}.json`,
+        JSON.stringify(userData, null, 2),
+        (err) => console.log(err));
+
+};
+
+
+app.post('/addUser', (req, res) => {
+    const { userName, activisionId, platform } = req.body;
+    if (!userName || !activisionId || !platform)
+        return res.status(400).send('missing information');
+
+    const userData = {
+        userCredentials: {
+            userName,
+            activisionId,
+            platform,
+        },
+        stats: {}
+    };
+    saveUserDataToAFile(userData);
+    res.send(`User ${userData.userCredentials.userName} has been created successfully`);
+});
 
 
 app.post('/userStats', async (req, res) => {
     const { userName, activisionId, platform } = req.body;
-    if (!userName || !activisionId || !platform) return res.status(400).send('missing information');
+    if (!userName || !activisionId || !platform)
+        return res.status(400).send('missing information');
 
     try {
         const { data } = await axios.get(`https://call-of-duty-vanguard.p.rapidapi.com/${platform}/user/${userName}#${activisionId}`, {
